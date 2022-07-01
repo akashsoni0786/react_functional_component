f5922df4e8a5f8483245387e616bff7086c1ef5a
akashsoni0786 <akashsoni@cedcoss.com>
2022-07-01 09:32:19 +0530

    Make a functional component as child of App.js and pass your name as prop, then "Hello <name>" must be printed.

---
+++ func3-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  var name = "Akash";
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello {name}</h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
