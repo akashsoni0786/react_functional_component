e6ac1af8cf824a78709790454d36544e3a5fea3b
akashsoni0786 <akashsoni@cedcoss.com>
2022-07-05 19:30:03 +0530

    Done

--- func6-app/src/App.js
+++ func6-app/src/App.js
@@ -1,31 +1,49 @@
-import './App.css';
-import React from 'react';
-import { Navbar,ProfileDashboard,Trends,Feeds,HowToFollow } from './Compo';
-class App extends React.Component{
-  render(){
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='divide'>
-      <div style={{width:"26%",float:"left"}}>
-      <br />
-      <ProfileDashboard />
-      <br />
-      <Trends />
-      </div>
-      <div style={{height:"460px",width:"26%",float:"left"}}>
-      <br />
-      <Feeds />
-      </div>
-      <div style={{width:"26%",float:"left"}}>
-      <br />
-      <HowToFollow />
-      </div>
-      </div>
-      
-    </div>
-  );
-}
+import "./App.css";
+import React from "react";
+import { Navbar, ProfileDashboard, Trends, Feeds, HowToFollow } from "./Compo";
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="main">
+          <Navbar />
+          <div className="section">
+              <div className="left">
+                <ProfileDashboard />
+                <Trends />
+              </div>
+              <div className="center">
+                <Feeds/>
+              </div>
+              <div className="right">
+                <HowToFollow />
+              </div>
+
+          </div>
+        </div>
+      </>
+      // <div style={{width:"90%",margin:"auto"}}>
+      //   <Navbar />
+      //   <div className='divide'>
+      //   <div style={{width:"25%",float:"left"}}>
+      //   <br />
+      //   <ProfileDashboard />
+      //   <br />
+      //   <Trends />
+      //   </div>
+      //   <div style={{height:"460px",width:"25%",float:"left"}}>
+      //   <br />
+      //   <Feeds />
+      //   </div>
+      //   <div style={{width:"25%",float:"left"}}>
+      //   <br />
+      //   <HowToFollow />
+      //   </div>
+      //   </div>
+
+      // </div>
+    );
+  }
 }
 
 export default App;
--- func6-app/src/Compo.js
+++ func6-app/src/Compo.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React from "react";
 
-export const Navbar=()=>{
-    return(
-        <div style={{backgroundColor:"#61dafb",width:"96.5%",padding:"20px"}}>Navbar</div>
-    );
-}
+export const Navbar = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#61dafb",
+        padding: "20px",
+      }}
+    >
+      Navbar
+    </div>
+  );
+};
 
-export const ProfileDashboard=()=>{
-    return(
-        <div style={{backgroundColor:"#61dafb",width:"90%",height:"200px",padding:"20px",marginRight:"1%"}}>Profile Dashboard</div>
-    );
-}
+export const ProfileDashboard = () => {
+  return (
+    <div className="dashboard">
+      Profile Dashboard
+    </div>
+  );
+};
 
-export const Trends=()=>{
-    return(
-        <div style={{backgroundColor:"#61dafb",width:"90%",height:"200px",padding:"20px",marginRight:"1%"}}>Trends</div>
-    );
-}
+export const Trends = () => {
+  return (
+    <div className="trends">
+      Trends
+    </div>
+  );
+};
 
-export const Feeds=()=>{
-    return(
-        <div style={{backgroundColor:"#61dafb",width:"90%",height:"100%",padding:"20px",marginRight:"1%"}}>Feeds</div>
-    );
-}
-export const HowToFollow=()=>{
-    return(
-        <div style={{backgroundColor:"#61dafb",width:"90%",height:"200px",padding:"20px",marginRight:"1%"}}>How To Follow</div>
-    );
-}
+export const Feeds = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "#61dafb",
+        // width: "20vw",
+        // height: "100%",
+        // padding: "20px",
+        // marginRight: "1%",
+      }}
+    >
+      Feeds
+    </div>
+  );
+};
+export const HowToFollow = () => {
+  return (
+    <div
+      style={
+        {
+          backgroundColor: "#61dafb",
+          // width: "20vw",
+          // height: "200px",
+          // padding: "20px",
+          // marginRight: "1%",
+        }
+      }
+    >
+      How To Follow
+    </div>
+  );
+};
